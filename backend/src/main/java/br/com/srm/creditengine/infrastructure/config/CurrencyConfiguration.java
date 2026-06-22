5231a47eb75130cfa2cadb77a4cd5552f125f55a
Danilo Franco <engdanilofranco@gmail.com>
2026-06-22 03:20:06 -0300

    feat(config): expose REST API dependencies and currency use case beans

--- backend/src/main/java/br/com/srm/creditengine/infrastructure/config/CurrencyConfiguration.java
+++ backend/src/main/java/br/com/srm/creditengine/infrastructure/config/CurrencyConfiguration.java
@@ -1,7 +1,11 @@
 package br.com.srm.creditengine.infrastructure.config;
 
+import br.com.srm.creditengine.application.currency.FindLatestExchangeRateUseCase;
+import br.com.srm.creditengine.application.currency.RegisterExchangeRateUseCase;
 import br.com.srm.creditengine.domain.currency.ExchangeRateLookupService;
 import br.com.srm.creditengine.domain.currency.ExchangeRateProvider;
+import br.com.srm.creditengine.infrastructure.persistence.jpa.CurrencyRepository;
+import br.com.srm.creditengine.infrastructure.persistence.jpa.ExchangeRateRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,5 +15,17 @@ public class CurrencyConfiguration {
     @Bean
     public ExchangeRateLookupService exchangeRateLookupService(ExchangeRateProvider exchangeRateProvider) {
         return new ExchangeRateLookupService(exchangeRateProvider);
+    }
+
+    @Bean
+    public RegisterExchangeRateUseCase registerExchangeRateUseCase(
+            CurrencyRepository currencyRepository, ExchangeRateRepository exchangeRateRepository) {
+        return new RegisterExchangeRateUseCase(currencyRepository, exchangeRateRepository);
+    }
+
+    @Bean
+    public FindLatestExchangeRateUseCase findLatestExchangeRateUseCase(
+            ExchangeRateLookupService exchangeRateLookupService) {
+        return new FindLatestExchangeRateUseCase(exchangeRateLookupService);
     }
 }
